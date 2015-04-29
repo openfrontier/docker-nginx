4f583f1a4ef5e58c76cb354e58e8b335e48e9e8d
zsx <thinkernel@gmail.com>
2015-04-29 15:03:25 +0800

    The ability to proxy more than one set of ci servers.

--- createNginx.sh
+++ createNginx.sh
@@ -4,6 +4,12 @@ GERRIT_NAME=${GERRIT_NAME:-gerrit}
 JENKINS_NAME=${JENKINS_NAME:-jenkins-master}
 NGINX_IMAGE_NAME=${NGINX_IMAGE_NAME:-nginx}
 NGINX_NAME=${NGINX_NAME:-nginx-proxy}
+PROXY_CONF=proxy.conf
 
-docker run --name ${NGINX_NAME} --link ${GERRIT_NAME}:gerrit --link ${JENKINS_NAME}:jenkins -p 80:80 -v ~/nginx-docker/proxy.conf:/etc/nginx/conf.d/proxy.conf:ro -d ${NGINX_IMAGE_NAME}
+# Setup proxy URI
+sed "s/{GERRIT_URI}/${GERRIT_NAME}/g" ~/nginx-docker/${PROXY_CONF}.template > ~/nginx-docker/${PROXY_CONF}
+sed -i "s/{JENKINS_URI}/${JENKINS_NAME}/g" ~/nginx-docker/${PROXY_CONF}
+
+# Start proxy
+docker run --name ${NGINX_NAME} --link ${GERRIT_NAME}:${GERRIT_NAME} --link ${JENKINS_NAME}:${JENKINS_NAME} -p 80:80 -v ~/nginx-docker/${PROXY_CONF}:/etc/nginx/conf.d/proxy.conf:ro -d ${NGINX_IMAGE_NAME}
 
