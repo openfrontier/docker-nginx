9f98da26784c0bc008980520ab02c260cedd4b07
zsx <thinkernel@gmail.com>
2015-06-10 09:39:42 +0000

    Support local nexus docker options.

--- createNginx.sh
+++ createNginx.sh
@@ -11,20 +11,34 @@ NGINX_NAME=${NGINX_NAME:-nginx-proxy}
 PROXY_CONF=proxy.conf
 
 # Setup proxy URI
-sed "s/{{HOST_URL}}/${HOST_NAME}/g" ~/nginx-docker/${PROXY_CONF}.template > ~/nginx-docker/${PROXY_CONF}
+if [ ${#NEXUS_WEBURL} -eq 0 ]; then
+    sed "s/{{HOST_URL}}/${HOST_NAME}/g" ~/nginx-docker/${PROXY_CONF}.nexus.template > ~/nginx-docker/${PROXY_CONF}
+else
+    sed "s/{{HOST_URL}}/${HOST_NAME}/g" ~/nginx-docker/${PROXY_CONF}.template > ~/nginx-docker/${PROXY_CONF}
+fi
 sed -i "s/{GERRIT_URI}/${GERRIT_NAME}/g" ~/nginx-docker/${PROXY_CONF}
 sed -i "s/{JENKINS_URI}/${JENKINS_NAME}/g" ~/nginx-docker/${PROXY_CONF}
 sed -i "s/{REDMINE_URI}/${REDMINE_NAME}/g" ~/nginx-docker/${PROXY_CONF}
 sed -i "s/{NEXUS_URI}/${NEXUS_NAME}/g" ~/nginx-docker/${PROXY_CONF}
 
 # Start proxy
-docker run \
---name ${NGINX_NAME} \
---link ${GERRIT_NAME}:${GERRIT_NAME} \
---link ${JENKINS_NAME}:${JENKINS_NAME} \
---link ${REDMINE_NAME}:${REDMINE_NAME} \
---link ${NEXUS_NAME}:${NEXUS_NAME} \
--p 80:80 \
--v ~/nginx-docker/${PROXY_CONF}:/etc/nginx/conf.d/default.conf:ro \
--d ${NGINX_IMAGE_NAME}
-
+if [ ${#NEXUS_WEBURL} -eq 0 ]; then #proxy nexus
+    docker run \
+    --name ${NGINX_NAME} \
+    --link ${GERRIT_NAME}:${GERRIT_NAME} \
+    --link ${JENKINS_NAME}:${JENKINS_NAME} \
+    --link ${REDMINE_NAME}:${REDMINE_NAME} \
+    --link ${NEXUS_NAME}:${NEXUS_NAME} \
+    -p 80:80 \
+    -v ~/nginx-docker/${PROXY_CONF}:/etc/nginx/conf.d/default.conf:ro \
+    -d ${NGINX_IMAGE_NAME}
+else #without nexus
+    docker run \
+    --name ${NGINX_NAME} \
+    --link ${GERRIT_NAME}:${GERRIT_NAME} \
+    --link ${JENKINS_NAME}:${JENKINS_NAME} \
+    --link ${REDMINE_NAME}:${REDMINE_NAME} \
+    -p 80:80 \
+    -v ~/nginx-docker/${PROXY_CONF}:/etc/nginx/conf.d/default.conf:ro \
+    -d ${NGINX_IMAGE_NAME}
+fi
