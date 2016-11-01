8f5ae356a34bcaf5d60d820a9ee2b145d2ca6f7b
thinkernel <thinkernel@gmail.com>
2016-11-01 11:18:14 +0000

    Make nginx self initializable for docker compose

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM nginx:stable-alpine
+
+MAINTAINER zsx <thinkernel@gmail.com>
+
+RUN set -x \
+    && apk add --update --no-cache bash curl
+
+COPY nginx-index.html /usr/share/nginx/html/index.html
+COPY proxy.conf /etc/nginx/conf.d/default.conf
+COPY nginx-entrypoint.sh /usr/local/bin/nginx-entrypoint.sh
+RUN chmod +x /usr/local/bin/nginx-entrypoint.sh
+
+ENTRYPOINT ["/usr/local/bin/nginx-entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
---
+++ nginx-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+PROXY_CONF=/etc/nginx/conf.d/default.conf
+
+if [ "$1" = "nginx" ]; then
+  # Replace position holders in default.conf
+  sed -i "s#{{SERVER_NAME}}#${SERVER_NAME}#g" ${PROXY_CONF}
+  sed -i "s#{{CLIENT_MAX_BODY_SIZE}}#${CLIENT_MAX_BODY_SIZE}#g" ${PROXY_CONF}
+  echo "====== ${PROXY_CONF} ======="
+  cat "${PROXY_CONF}"
+  echo "============================"
+
+  SLEEP_TIME=5
+  declare -a DEPENDENCIES=( "gerrit:8080/gerrit" "jenkins:8080/jenkins" )
+  for d in ${DEPENDENCIES[@]}; do 
+    echo "waiting for $d to be available";
+    # use curl as already installed... 
+    # We are checking for response codes that are not of class 5xx the most common are below, the list does not 
+    # try to be exaustive, it only consider the response code that will guarantee NGINX to start when all dependencies are 
+    # available.
+    until curl --location --output /dev/null --silent --write-out "%{http_code}\\n" http://$d | grep "200\|404\|403\|401\|301\|302" &> /dev/null
+    do
+      echo "$d unavailable, sleeping for ${SLEEP_TIME}"
+      sleep "${SLEEP_TIME}"
+    done
+    echo "$d ready"
+  done
+fi
+
+exec "$@"
